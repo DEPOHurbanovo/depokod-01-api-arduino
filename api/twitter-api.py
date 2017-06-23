97f667c5dc56ee456bf069e24b6c15bfc7f9c603
Peter Nagy <xnagyp01@stud.fit.vutbr.cz>
2017-06-23 20:29:16 +0200

    Translate the tweet and translate to morse code and prin

--- api/twitter-api.py
+++ api/twitter-api.py
@@ -1,6 +1,25 @@
 import tweepy
 from tweepy import OAuthHandler
 import os
+import goslate
+import sys
+
+# Morse code translation table
+CODE = {'A': '.-',     'B': '-...',   'C': '-.-.', 
+        'D': '-..',    'E': '.',      'F': '..-.',
+        'G': '--.',    'H': '....',   'I': '..',
+        'J': '.---',   'K': '-.-',    'L': '.-..',
+        'M': '--',     'N': '-.',     'O': '---',
+        'P': '.--.',   'Q': '--.-',   'R': '.-.',
+     	'S': '...',    'T': '-',      'U': '..-',
+        'V': '...-',   'W': '.--',    'X': '-..-',
+        'Y': '-.--',   'Z': '--..',
+        
+        '0': '-----',  '1': '.----',  '2': '..---',
+        '3': '...--',  '4': '....-',  '5': '.....',
+        '6': '-....',  '7': '--...',  '8': '---..',
+        '9': '----.' 
+        }
 
 consumer_key = os.environ['DEPOKOD_TWITTER_CONSUMER_KEY']
 consumer_secret = os.environ['DEPOKOD_TWITTER_CONSUMER_SECRET']
@@ -10,7 +29,17 @@ access_secret = os.environ['DEPOKOD_TWITTER_ACCESS_SECRET']
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
+# Access and read the tweet
 api = tweepy.API(auth)
+tweet = api.user_timeline('chuck_facts')[1]
 
-tweet = api.user_timeline('elonmusk')[0]
-print(tweet.text)
+# Translate the tweet
+gs = goslate.Goslate()
+translated = gs.translate(tweet.text, 'sk')
+
+api.update_status(translated)
+
+# Translate to morse code
+translated_raw = translated
+for char in translated_raw:
+		print(CODE[char.upper()])
